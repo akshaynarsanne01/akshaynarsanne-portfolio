b233b8c893cf8ffa07c0abb3277f6ac80486511e
Akshay Narsanne <147087536+akshaynarsanne01@users.noreply.github.com>
2024-12-03 21:52:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import './App.css'
 import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar';
+import BlogHome from './pages/BlogHome';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import CreatePost from './pages/CreatePost';
+import SinglePost from './components/SinglePost';
 import { useEffect } from 'react';
 function App() {
   useEffect(() => {
@@ -9,11 +16,18 @@ function App() {
     };
   }, []);
   return (
-    <>
-      <div className=''>
-        <Home />
+   <Router>
+      <Navbar />
+      <div className="container p-4 text-black">
+        <Routes>
+          <Route path="/" element={<BlogHome />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/create" element={<CreatePost />} />
+          <Route path="/post/:id" element={<SinglePost />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   )
 }
 
